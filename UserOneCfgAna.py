11176e923c1cfe8c2284c32e31d50c54c7a10c5c
vikrambverma <vikrambverma@yahoo.com>
2022-02-19 21:27:08 +0530

    Delete UserOneCfgAna.py

--- UserOneCfgAna.py
+++
@@ -1,7 +0,0 @@
-gv_Master_CfgChanged = False
-gv_Master_DeviceTimeout = 10
-gv_Master_UserTimeout = 60
-gv_Master_UserNames = ['admin', 'user2']
-gv_Master_UserPasswords = ['admin@1234', 'user2@1234']
-gv_Master_UserTypes = ['admin', 'user']
-gv_Master_UserDevMacs = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]]
